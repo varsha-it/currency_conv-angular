8ed694f8710d30b528802a71ef92892422bde2c5
varsha-it <113794449+varsha-it@users.noreply.github.com>
2023-02-06 12:08:15 +0530

    Add files via upload

---
+++ src/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CurrencyconvPipe } from './currencyconv.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrencyconvPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
